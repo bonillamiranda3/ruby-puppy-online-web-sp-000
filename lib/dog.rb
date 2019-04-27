69238dc296bbf5a7797d0ddfaf771722e7be1313
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-27 00:41:17 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+
+  def self.all
+      @@all.each do |dog|
+        puts dog.name
+  end
+    def self.clear_all
+      @@all.clear_all
+    end
+end
